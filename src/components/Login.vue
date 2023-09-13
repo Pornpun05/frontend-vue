f338e4fdeff2091a6d9fbdb1771b9a3c6d49eb53
Pornpun05 <6310210272@psu.ac.th>
2023-09-13 12:13:34 +0700

    lab9

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,63 +1,41 @@
 <template>
-  <v-form
-    ref="form"
-    v-model="valid"
-    lazy-validation
-  >
-    <v-text-field
-      v-model="name"
-      :counter="10"
-      :rules="nameRules"
-      label="Name"
-      required
-    ></v-text-field>
-
-    <v-text-field
-      v-model="email"
-      :rules="emailRules"
-      label="E-mail"
-      required
-    ></v-text-field>
-
-    <v-select
-      v-model="select"
-      :items="items"
-      :rules="[v => !!v || 'Item is required']"
-      label="Item"
-      required
-    ></v-select>
-
-    <v-checkbox
-      v-model="checkbox"
-      :rules="[v => !!v || 'You must agree to continue!']"
-      label="Do you agree?"
-      required
-    ></v-checkbox>
-
-    <v-btn
-      :disabled="!valid"
-      color="success"
-      class="mr-4"
-      @click="validate"
+  <v-card>
+    <v-card-title style="font-size: 24px ">เข้าสู่ระบบ</v-card-title>
+    <v-card-text>
+    <v-form
+      ref="loginForm"
+      v-model="valid"
+      lazy-validation
     >
-      Validate
-    </v-btn>
+      <v-text-field
+        v-model="name"
+        :counter="20"
+        :rules="nameRules"
+        label="ชื่อผู้ใช้งาน"
+        required
+        outlined
+      ></v-text-field>
 
-    <v-btn
-      color="error"
-      class="mr-4"
-      @click="reset"
-    >
-      Reset Form
-    </v-btn>
+      <v-text-field
+        v-model="password"
+        :rules="passwordRules"
+        label="รหัสผ่าน"
+        required
+        outlined
+      ></v-text-field>
 
-    <v-btn
-      color="warning"
-      @click="resetValidation"
-    >
-      Reset Validation
-    </v-btn>
-  </v-form>
+      <v-btn
+        :disabled="!valid"
+        color="success"
+        class="mr-4"
+        @click="Login"
+        block
+      >
+        เข้าสู่ระบบ
+      </v-btn>
+    </v-form>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
@@ -66,24 +44,23 @@ export default {
       valid: true,
       name: '',
       nameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+        v => !!v || 'กรุณากรอกชื่อผู้ใช้',
+        v => (v && v.length <= 20) || 'กรุณากรอกชื่อผู้ใช้งานห้ามเกิน 20ตัวอักษร',
       ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-      ],
-      select: null,
-      items: [
-        'Item 1',
-        'Item 2',
-        'Item 3',
-        'Item 4',
-      ],
-      checkbox: false,
+      password: '',
+      passwordRules: [
+        v => !!v || 'กรุณากรอกรหัสผ่าน',
+      ] 
     }),
-
+    methods: {
+      Login () {
+        if (this.$refs.loginForm.validate(true)) {
+          localStorage.setItem('username', this.name)
+          this.$EventBus.$emit('getUsername')
+          this.$router.push('/')
+        }
+      }
+    }
 }
 </script>
 
